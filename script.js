b0ee0b98e53cb87c75bafa460863493d3f36e953
ak1303 <94529241+ak1303@users.noreply.github.com>
2024-02-27 01:00:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function validEmail(str) {
   //your JS code here.
+	let arr1= str.split('@');
+  if(arr1.length!=2)return false;
+  if(arr1[0].length==0)return false;
+
+  let arr2 = arr1[1].split('.');
+  if(arr2[0].length==0)return false;
+  for(let i=1;i<arr2.length;i++){
+    if(arr2[i].length!=2 && arr2[i].length!=3)return false;
+  }
+  return true;
 }
 
 // Do not change the code below.
